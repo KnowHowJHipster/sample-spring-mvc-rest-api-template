8ff98fd7c61c74764517ec6e8ebfb1ba73530606
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-03-01 11:50:10 +0200

    refactor: use application-settings prefix for config keys

--- src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/AuthenticationIntegrationTest.java
+++ src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/AuthenticationIntegrationTest.java
@@ -34,8 +34,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(
     properties = {
-        "iqkv.security.authentication.jwt.base64-secret=fd54a45s65fds737b9aafcb3412e07ed99b267f33413274720ddbb7f6c5e64e9f14075f2d7ed041592f0b7657baf8",
-        "iqkv.security.authentication.jwt.token-validity-in-seconds=60000",
+        "application-settings.security.authentication.jwt.base64-secret=fd54a45s65fds737b9aafcb3412e07ed99b267f33413274720ddbb7f6c5e64e9f14075f2d7ed041592f0b7657baf8",
+        "application-settings.security.authentication.jwt.token-validity-in-seconds=60000",
     },
     classes = {
         ApplicationProperties.class,
